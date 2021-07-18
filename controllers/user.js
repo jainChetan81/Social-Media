c7712ffd63a91b47a7bf2dc8a38cc0154e931a77
jainChetan81 <jain.cj.chetan@gmail.com>
2021-07-18 23:01:42 +0530

    added user CRUD API's

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const User = mongoose.model("User");
 
 exports.userById = (req, res, next, id) => {
@@ -8,15 +9,41 @@ exports.userById = (req, res, next, id) => {
 		next();
 	});
 };
+
 exports.hasAuthorization = (req, res, next) => {
 	const authorized = req.profile && req.auth && req.profile._id === req.auth._id;
 	if (!authorized)
 		return res.status(403).json({ error: "User is not authorized to perfor this action!" });
 	next();
 };
+
 exports.allUsers = (req, res, next) => {
 	User.find((err, users) => {
 		if (err) return res.status(400).json({ error: err });
 		res.json(users);
 	}).select(" name email updated created");
+};
+exports.getUser = (req, res) => {
+	const { created, _id, name, email } = req.profile;
+	res.json({ created, _id, name, email });
+};
+
+//function to update current user profile
+exports.updateUser = (req, res, next) => {
+	let user = req.profile;
+	user = _.extend(user, req.body); //extend- mutate the user object with req.body
+	user.updated = Date.now();
+	user.save((err) => {
+		if (err) return res.status(400).json({ error: err });
+		res.json(user);
+	});
+};
+
+//function to delete current user profile
+exports.deleteUser = (req, res, next) => {
+	let user = req.profile;
+	user.remove((err) => {
+		if (err) return res.status(400).json({ error: err });
+		res.json({ message: "User has been deleted successfully" });
+	});
 };
